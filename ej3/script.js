a44be78227eee32a76e4f21523a297678f42d350
DESKTOP-79VDJDE\Weslin <weslinsilva2017@gmail.com>
2022-04-23 22:54:51 -0600

    Terminado ejercicio 3

--- ej3/script.js
+++ ej3/script.js
@@ -1,17 +1,4 @@
-var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-var dni; 
-var letra; 
-var res; 
-dni = parseInt(prompt("Ingrese su dni: "));
-letra = prompt("Ingrese la letra de su dni");
-if(dni < 0 || dni > 99999999){
-    alert("El número ingresado no es válido");
-}else{
-    var res  ; 
-    res = letras[dni % 23];
-    if(res === letra ){
-        alert("La letra y el numero ingresado son correctos");
-    }else{
-        alert("La letra y el número no coinciden");
-    }
+var meses =["ENERO","FEBRERO","MARZO", "ABRIL", "MAYO", "JUNIO","JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
+for(let i = 0; i < meses.length; i++){
+    alert(meses[i]);
 }
