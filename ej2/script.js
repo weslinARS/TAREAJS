97c4c152a17c2751879759989d55a0c3fd11760d
DESKTOP-79VDJDE\Weslin <weslinsilva2017@gmail.com>
2022-04-23 22:46:57 -0600

    Terminado script ejercicio2

--- ej2/script.js
+++ ej2/script.js
@@ -1,4 +1,2 @@
-var meses_array = ["ENERO", "FEBRERO", "ABRIL", "MAYO", "JUNIO" ,"JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
-for (let i = 0; i < meses_array.length; i++) {
-    alert(meses_array[i])
-}
+var mensaje  = "Hola Mundo!\nQué fácil es incluir \'comillas simples\'\n y \"Comillas dobles\"";
+alert(mensaje);
